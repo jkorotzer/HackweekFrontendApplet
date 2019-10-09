bd4e2fde697e3e702b666c3548b41bdc762a3701
Jared Korotzer <jkorotzer@squareup.com>
2019-10-09 16:20:19 -0700

    Update UI and add frontend functionality

--- src/main/HackweekApplet.java
+++ src/main/HackweekApplet.java
@@ -2,31 +2,34 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+import javax.swing.JSeparator;
+import javax.swing.ListCellRenderer;
+import javax.swing.SwingConstants;
+import javax.swing.border.MatteBorder;
 import org.eclipse.jetty.server.Server;
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
-import org.jdesktop.swingx.autocomplete.ObjectToStringConverter;
 
 public class HackweekApplet {
-  JFrame frame = new JFrame("Hackweek Applet");
-  HackweekPanel topPanel = new HackweekPanel();
-  LowerPanel lowerPanel = new LowerPanel();
+  JFrame frame = new JFrame("");
+  LowerHackweekPanel lowerPanel = new LowerHackweekPanel();
+  UpperHackweekPanel upperPanel = new UpperHackweekPanel(this);
   JPanel outerPanel;
 
 
@@ -37,92 +40,210 @@ public class HackweekApplet {
     frame.setLayout(layout);
     frame.setSize(300, 100);
     outerPanel = new JPanel(new BorderLayout());
-    outerPanel.add(topPanel, BorderLayout.NORTH);
+    outerPanel.add(upperPanel, BorderLayout.NORTH);
+    outerPanel.add(new JSeparator(SwingConstants.VERTICAL));
     outerPanel.add(lowerPanel, BorderLayout.CENTER);
     frame.add(outerPanel);
     frame.setVisible(true);
-    frame.getContentPane().setBackground(Color.BLACK);
-    frame.getContentPane().setPreferredSize(new Dimension(300, 150));
-  }
-
-  public void addLowerPanel(String text, ImageIcon icon) {
-    lowerPanel.setTextAndIcon(text, icon);
+    frame.getContentPane().setBackground(Color.WHITE);
     frame.pack();
-    frame.revalidate();
-    frame.repaint();
   }
 
-  public class LowerPanel extends JPanel {
-    JLabel nameLabel = new JLabel();
-    JLabel appLabel = new JLabel();
+  public void redrawNameAndFace(String name) {
+    lowerPanel.redrawNameAndFace(name);
+  }
 
-    public LowerPanel() {
-      nameLabel.setForeground(Color.WHITE);
-      nameLabel.setVisible(false);
-      this.add(nameLabel);
+  public class UpperHackweekPanel extends JPanel {
+    private HackweekApplet applet;
+    String[] NAMES = {"Samir", "Tami", "Ryan", "Jared", "Jack"};
+    JComboBox comboBox = new JComboBox(NAMES);
+    private BufferedImage currentUserIcon;
+    String currentUserIconPath = "src/main/resources/samir.png"; // Change this to use a different user
+    JLabel userIconLabel;
 
-      appLabel.setVisible(false);
-      this.add(appLabel);
+    public UpperHackweekPanel(HackweekApplet applet) {
+      this.applet = applet;
+      try {
+        File file = new File(currentUserIconPath);
+        if(!file.exists()) {
+          System.err.println("my file is not there, I was looking at " + file.getAbsolutePath());
+        }
+        currentUserIcon = ImageIO.read(file);
+      } catch (IOException ex) {
+        System.out.println("No appIcon");
+      }
+
+      AutoCompleteDecorator.decorate(comboBox);
+      comboBox.setPrototypeDisplayValue("Please search here.       ");
+      for (Component component : comboBox.getComponents()) {
+        if (component instanceof JButton) {
+          comboBox.remove(component);
+        }
+      }
+      comboBox.setRenderer(new ComboBoxRenderer());
+      comboBox.addActionListener (e -> {
+        String selectedValue = comboBox.getSelectedItem().toString();
+        applet.redrawNameAndFace(selectedValue);
+      });
+      add(comboBox);
+
+      userIconLabel = new JLabel(new ImageIcon(currentUserIcon.getScaledInstance(30, 30, Image.SCALE_FAST)));
+      this.add(userIconLabel);
 
       setOpaque(true);
-      setBackground(Color.BLACK);
+      setBackground(Color.WHITE);
     }
 
-    public void setTextAndIcon(String text, ImageIcon icon) {
-      nameLabel.setText(text);
-      appLabel.setIcon(icon);
-      nameLabel.setVisible(true);
-      appLabel.setVisible(true);
-      frame.revalidate();
-      frame.repaint();
+    public class ComboBoxRenderer extends JLabel implements ListCellRenderer {
+      private JLabel label = new JLabel();
+      private JPanel panel = new JPanel();
+
+      public ComboBoxRenderer() {
+
+        label.setOpaque(false);
+        label.setFont(new Font("Helvetica Neue", Font.PLAIN, 12));
+        label.setForeground(Color.BLACK);
+
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 2));
+        panel.add(label);
+      }
+
+      @Override
+      public Component getListCellRendererComponent(JList list, Object value,
+          int index, boolean isSelected, boolean cellHasFocus) {
+        setText(value.toString());
+        return this;
+      }
     }
   }
 
-  public class HackweekPanel extends JPanel {
-    private BufferedImage intelliJImage;
+  public static class LowerHackweekPanel extends JPanel {
+    private BufferedImage appIcon;
+    private BufferedImage userIcon;
+    private BufferedImage phoneIcon;
+    private BufferedImage messageIcon;
+    String messageIconPath = "src/main/resources/message_icon.png";
+    String phoneImagePath = "src/main/resources/phone-icon.png";
     String imageFilePath = "src/main/resources/IntelliJ_IDEA_Logo.png";
-    String[] NAMES = {"Samir", "Tami", "Ryan", "Jared", "Jack"};
-    HashSet<String> namesSet = new HashSet<>(Arrays.asList(NAMES));
-    JTextField textField = new JTextField(10);
+    String userIconPath = "src/main/resources/jared.png";
+    HashMap<String, String> namesToImagePaths = new HashMap<>();
+    HashMap<String, String> namesToAppPaths = new HashMap<>();
+    JLabel userIconLabel;
+    JLabel usernameLabel;
+    JLabel appIconLabel;
 
-    public HackweekPanel() {
+    public LowerHackweekPanel() {
+      initializeNamesToImagesMap();
+      initializeNamesToAppsMap();
+
       try {
         File file = new File(imageFilePath);
         if(!file.exists()) {
           System.err.println("my file is not there, I was looking at " + file.getAbsolutePath());
         }
-        intelliJImage = ImageIO.read(file);
+        appIcon = ImageIO.read(file);
       } catch (IOException ex) {
-        System.out.println("No intelliJImage");
+        System.out.println("No appIcon");
       }
 
-      JList namesList = new JList(NAMES);
-      AutoCompleteDecorator.decorate(namesList, textField, ObjectToStringConverter.DEFAULT_IMPLEMENTATION);
-      textField.getDocument().addDocumentListener(new DocumentListener() {
-        public void changedUpdate(DocumentEvent e) {
-          checkNameAndRedrawIfPresent();
+      try {
+        File file = new File(userIconPath);
+        if(!file.exists()) {
+          System.err.println("my file is not there, I was looking at " + file.getAbsolutePath());
         }
-        public void removeUpdate(DocumentEvent e) {
-          checkNameAndRedrawIfPresent();
-        }
-        public void insertUpdate(DocumentEvent e) {
-          checkNameAndRedrawIfPresent();
-        }
+        userIcon = ImageIO.read(file);
+      } catch (IOException ex) {
+        System.out.println("No user icon");
+      }
 
-        public void checkNameAndRedrawIfPresent() {
-          String s = textField.getText();
-          if(namesSet.contains(textField.getText())) {
-            addLowerPanel(s, new ImageIcon(intelliJImage.getScaledInstance(50, 50, Image.SCALE_FAST)));
-          }
+      try {
+        File file = new File(phoneImagePath);
+        if(!file.exists()) {
+          System.err.println("my file is not there, I was looking at " + file.getAbsolutePath());
         }
-      });
-      this.add(textField);
+        phoneIcon = ImageIO.read(file);
+      } catch (IOException ex) {
+        System.out.println("No user icon");
+      }
 
-      JLabel picLabel = new JLabel(new ImageIcon(intelliJImage.getScaledInstance(50, 50, Image.SCALE_FAST)));
-      this.add(picLabel);
+      try {
+        File file = new File(messageIconPath);
+        if(!file.exists()) {
+          System.err.println("my file is not there, I was looking at " + file.getAbsolutePath());
+        }
+        messageIcon = ImageIO.read(file);
+      } catch (IOException ex) {
+        System.out.println("No user icon");
+      }
+
+      userIconLabel = new JLabel(new ImageIcon(userIcon.getScaledInstance(30, 30, Image.SCALE_FAST)));
+      this.add(userIconLabel);
+
+      usernameLabel = new JLabel("Jared");
+      usernameLabel.setFont(new Font("Helvetica Neue", Font.PLAIN, 16));
+      usernameLabel.setForeground(Color.BLACK);
+      usernameLabel.setBorder(new MatteBorder(0, 0, 1, 0, Color.BLACK));
+      usernameLabel.setMinimumSize(new Dimension(75, 20));
+      usernameLabel.setPreferredSize(new Dimension(75, 20));
+      usernameLabel.setMaximumSize(new Dimension(75, 20));
+      add(usernameLabel);
+
+      JLabel messageLabel = new JLabel(new ImageIcon(messageIcon.getScaledInstance(30, 30, Image.SCALE_FAST)));
+      add(messageLabel);
+
+      JLabel phoneLabel = new JLabel(new ImageIcon(phoneIcon.getScaledInstance(30, 30, Image.SCALE_FAST)));
+      add(phoneLabel);
+
+      appIconLabel = new JLabel(new ImageIcon(appIcon.getScaledInstance(30, 30, Image.SCALE_FAST)));
+      add(appIconLabel);
+      //add(new JSeparator(SwingConstants.VERTICAL));
 
       setOpaque(true);
-      setBackground(Color.BLACK);
+      setBackground(Color.WHITE);
+    }
+
+    public void initializeNamesToImagesMap() {
+      namesToImagePaths.put("Samir", "src/main/resources/samir.png");
+      namesToImagePaths.put("Jared", "src/main/resources/jared.png");
+      namesToImagePaths.put("Tami", "src/main/resources/tami.png");
+      namesToImagePaths.put("Ryan", "src/main/resources/ryan.png");
+      namesToImagePaths.put("Jack", "src/main/resources/jack.png");
+    }
+
+    public void initializeNamesToAppsMap() {
+      namesToAppPaths.put("Samir", "src/main/resources/IntelliJ_IDEA_Logo.png");
+      namesToAppPaths.put("Jared", "src/main/resources/IntelliJ_IDEA_Logo.png");
+      namesToAppPaths.put("Tami", "src/main/resources/rubymine-icon.png");
+      namesToAppPaths.put("Ryan", "src/main/resources/IntelliJ_IDEA_Logo.png");
+      namesToAppPaths.put("Jack", "src/main/resources/sublime_icon.png");
+    }
+
+    public void redrawNameAndFace(String name) {
+      String imagePath = namesToImagePaths.get(name);
+      try {
+        File file = new File(imagePath);
+        if(!file.exists()) {
+          System.err.println("my file is not there, I was looking at " + file.getAbsolutePath());
+        }
+        userIcon = ImageIO.read(file);
+      } catch (IOException ex) {
+        System.out.println("No image");
+      }
+
+      String appPath = namesToAppPaths.get(name);
+      try {
+        File file = new File(appPath);
+        if(!file.exists()) {
+          System.err.println("my file is not there, I was looking at " + file.getAbsolutePath());
+        }
+        appIcon = ImageIO.read(file);
+      } catch (IOException ex) {
+        System.out.println("No image");
+      }
+
+      userIconLabel.setIcon(new ImageIcon(userIcon.getScaledInstance(30, 30, Image.SCALE_FAST)));
+      appIconLabel.setIcon(new ImageIcon(appIcon.getScaledInstance(30, 30, Image.SCALE_FAST)));
+      usernameLabel.setText(name);
     }
   }
 
